f0741ed51182d21e98bc2d98b114b5832a82ed48
VinayTeja488 <113350523+VinayTeja488@users.noreply.github.com>
2023-11-23 08:42:53 +0530

    10.playfair matrix.c

--- 10.playfair matrix.c
+++ 10.playfair matrix.c
@@ -11,9 +11,9 @@ char playfairMatrix[SIZE][SIZE] = {
     {'D', 'S', 'T', 'B', 'C'}
 };
 
-// Function to find the position of a given character in the Playfair matrix
+
 void findPosition(char ch, int *row, int *col) {
-    if (ch == 'J') // I and J are usually treated as the same letter in Playfair cipher
+    if (ch == 'J') 
         ch = 'I';
 
     for (*row = 0; *row < SIZE; (*row)++) {
@@ -24,43 +24,42 @@ void findPosition(char ch, int *row, int *col) {
     }
 }
 
-// Function to encrypt a digraph using the Playfair matrix
+
 void encryptDigraph(char digraph[2], char cipher[2]) {
     int row1, col1, row2, col2;
 
     findPosition(digraph[0], &row1, &col1);
     findPosition(digraph[1], &row2, &col2);
 
-    // Same row
+    
     if (row1 == row2) {
         cipher[0] = playfairMatrix[row1][(col1 + 1) % SIZE];
         cipher[1] = playfairMatrix[row2][(col2 + 1) % SIZE];
     }
-    // Same column
+    
     else if (col1 == col2) {
         cipher[0] = playfairMatrix[(row1 + 1) % SIZE][col1];
         cipher[1] = playfairMatrix[(row2 + 1) % SIZE][col2];
     }
-    // Different row and column
+  
     else {
         cipher[0] = playfairMatrix[row1][col2];
         cipher[1] = playfairMatrix[row2][col1];
     }
 }
 
-// Function to encrypt a message using Playfair cipher
+
 void playfairEncrypt(char *message) {
     int i, j;
     char digraph[2], cipher[2];
 
-    // Adjusting the message (e.g., handling 'J' and 'I')
     for (i = 0; i < strlen(message); i += 2) {
         if (message[i] == message[i + 1]) {
             message[i + 1] = 'X';
         }
     }
 
-    // Encrypting the message
+   
     for (i = 0; i < strlen(message); i += 2) {
         digraph[0] = message[i];
         digraph[1] = message[i + 1];
